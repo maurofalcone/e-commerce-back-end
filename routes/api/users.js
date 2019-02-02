10083ce05fea0d31940964fc4e7157f5cb7228c0
maurofalcone <maurofalcone.95@gmail.com>
2019-02-02 14:50:59 -0300

    created user and hash password

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,3 +8,38 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 // Load User model
 const user = require("../../models/User");
+
+router.post("/register", (req, res) => {
+  // Form validation
+const { errors, isValid } = validateRegisterInput(req.body);
+// Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+//method to find if already a user exists
+user.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    }
+//creating a new user
+const newUser = new user({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+// Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
