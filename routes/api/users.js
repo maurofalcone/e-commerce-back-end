ee6e819b39edb9d7d7f72fa708decba4b4eaa80f
maurofalcone <maurofalcone.95@gmail.com>
2019-02-21 15:54:08 -0300

    decode jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const isEmpty= require('is-empty')
 const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 // Load input validation
@@ -14,26 +15,39 @@ router.post('/register', (req, res) => {
 const { errors, isValid } = validateRegisterInput(req.body)
 // Check validation
   if (!isValid) {
-    throw new Error(errors)
+    res.status(400).json(errors)
   }
   else {
     User.findOne({ email: req.body.email }).then(user => {
-      if (user !== null) {
-        throw new Error({ email: 'Email already exists' })
+      if (user) {
+        res.status(400).json({ email: 'Email already exists' })
       }
       else {
-          const newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+        let newId = 0;
+        User.find().limit(1).sort({$natural:-1}).then( user => {
+          if(isEmpty(user)) {
+            newId = (newId + 1)
+          }
+          else {
+            newId = (user[0].id + 1)
+          }
+            const newUser = new User({
+              id: newId,
+              name: req.body.name,
+              email: req.body.email,
+              password: req.body.password
+            })
+    // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err
+              newUser.password = hash
+              newUser.save().then(user => res.json(user)).catch(err => console.log(err))
+            })
           })
-  // Hash password before saving in database
-          bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err
-            newUser.password = hash
-            newUser.save().then(user => res.json(user)).catch(err => console.log(err))
-          })
+        })
+        .catch(error => {
+          res.status(400).json({message:"an error has occurred"})
         })
       }
     })
@@ -74,17 +88,28 @@ const { errors, isValid } = validateLoginInput(req.body)
             (err, token) => {
               res.json({
                 success: true,
-                token: 'Bearer ' + token
+                token: token
               })
             }
           )
         }
         else {
-          res.status(400).json({ passwordincorrect: 'Password incorrect' })
+          throw new Error({ error: 'Password incorrect' })
         }
       })
     }
   })
 })
+
+router.post('/checkJWT', (req, res) => {
+  const token = req.body.token
+  if(token) {
+    const data = jwt.decode(token)
+    return res.status(200).json({data})
+  }
+   else {
+     throw new Error({error:'Failed to login'})
+   }
+ })
 
 module.exports = router
